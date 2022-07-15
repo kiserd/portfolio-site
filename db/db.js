6c357ee82ab9530300e3b8930ae9e7126885a7e7
kiserd <kiserd@oregonstate.edu>
2022-07-15 08:31:19 -0700

    added connection to dynamo

---
+++ db/db.js
@@ -0,0 +1,11 @@
+import AWS from "aws-sdk";
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.REGION_NAME,
+});
+
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: 'latest' });
+
+export default db;
